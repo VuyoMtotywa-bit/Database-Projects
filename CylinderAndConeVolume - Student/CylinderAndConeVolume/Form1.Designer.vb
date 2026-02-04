1d6d026e1640e275792982147657dede6cdc0d31
Vuyo Mtotywa <mihlemtotywa@gmail.com>
2026-02-04 15:06:15 +0200

    Add files via upload

---
+++ CylinderAndConeVolume - Student/CylinderAndConeVolume/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.radCone = New System.Windows.Forms.RadioButton()
+        Me.radCircleArea = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblVolume = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtHeight)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 98)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(318, 142)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Figure:Input"
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(76, 80)
+        Me.txtHeight.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(132, 22)
+        Me.txtHeight.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 90)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 16)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Height:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.radCone)
+        Me.GroupBox2.Controls.Add(Me.radCircleArea)
+        Me.GroupBox2.Location = New System.Drawing.Point(343, 98)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(290, 142)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Figure:Type"
+        '
+        'radCone
+        '
+        Me.radCone.AutoSize = True
+        Me.radCone.Location = New System.Drawing.Point(21, 80)
+        Me.radCone.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.radCone.Name = "radCone"
+        Me.radCone.Size = New System.Drawing.Size(60, 20)
+        Me.radCone.TabIndex = 1
+        Me.radCone.TabStop = True
+        Me.radCone.Text = "Cone"
+        Me.radCone.UseVisualStyleBackColor = True
+        '
+        'radCircleArea
+        '
+        Me.radCircleArea.AutoSize = True
+        Me.radCircleArea.Location = New System.Drawing.Point(21, 36)
+        Me.radCircleArea.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.radCircleArea.Name = "radCircleArea"
+        Me.radCircleArea.Size = New System.Drawing.Size(94, 20)
+        Me.radCircleArea.TabIndex = 0
+        Me.radCircleArea.TabStop = True
+        Me.radCircleArea.Text = "Circle Area"
+        Me.radCircleArea.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label4.Location = New System.Drawing.Point(29, 242)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(602, 76)
+        Me.Label4.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(55, 270)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Volume"
+        '
+        'lblVolume
+        '
+        Me.lblVolume.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblVolume.Location = New System.Drawing.Point(120, 270)
+        Me.lblVolume.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblVolume.Name = "lblVolume"
+        Me.lblVolume.Size = New System.Drawing.Size(315, 28)
+        Me.lblVolume.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(445, 270)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(123, 16)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Square centimeters"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(76, 334)
+        Me.btnCalculate.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(100, 28)
+        Me.btnCalculate.TabIndex = 7
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(280, 334)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 28)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(648, 379)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.lblVolume)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtHeight As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents radCone As System.Windows.Forms.RadioButton
+    Friend WithEvents radCircleArea As System.Windows.Forms.RadioButton
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents lblVolume As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ CylinderAndConeVolume - Student/CylinderAndConeVolume/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Call ClearControls()
+    End Sub
+
+    Sub ClearControls()
+        'Clear Controls
+
+        txtHeight.Clear()
+        lblVolume.Text = ""
+        radCircleArea.Checked = True
+        txtHeight.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs)
+        Call LogOut()
+    End Sub
+    Sub LogOut()
+        Dim intResponse As Integer
+        intResponse = MessageBox.Show("Do you want to exit?", "Exit", MessageBoxButtons.YesNo,
+                                      MessageBoxIcon.Question)
+        If intResponse = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+
+
+
+End Class
